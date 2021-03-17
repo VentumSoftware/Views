55f6b06457233b1de530912ae993beebe138cd75
estebanenve <esteban@enve.io>
2021-03-17 17:44:09 -0300

    maps description

--- dashboard/maps/maps.js
+++ dashboard/maps/maps.js
@@ -1,169 +1,27 @@
 import utils from 'https://ventumdashboard.s3.amazonaws.com/lib/utils.js';
 import card from 'https://ventumdashboard.s3.amazonaws.com/dashboard/card/card.js';
 import buttons from 'https://ventumdashboard.s3.amazonaws.com/dashboard/buttons/buttons.js';
-import modal from 'https://ventumdashboard.s3.amazonaws.com/dashboard/modal/modal.js';
-import dashboard from 'https://ventumdashboard.s3.amazonaws.com/dashboard/dashboard.js';
 
-const dfltState = {
-    id: "noID",
-    title: "Mapas",
-    childs:{},
-    headers: {},
-    filters: {},
-    headerBtns: {}
-};
+//Caracteristicas de este componente (map)
+const component = {
+    //Dflt Modal State
+    dfltState: {
+        id: "noID",
+        title: "Mapas",
+        childs:{},
+        headers: {},
+        filters: {},
+        headerBtns: {},
+        childs: {},
+        html: {}
+    },
+    //Commandos específicos para el componente (map)
+    cmds: {},
+    //Typos de hijos que puede tener el componente (modal)
+    childTypes: ["modal"],
+    show: (state, parent) => {
 
-var states = [];
-
-//-----------------------------------------------------------------------------------------------
-
-const cmd = (state, cmds, res, pos) => {
-
-    const parentCmd = (state, payload, res) => {
-        switch (state.parentState.type) {
-            case "modal":
-                payload.cmds = payload.cmds || res;
-                return modal.cmd(state.parentState, payload.cmds, res, 0);
-            default:
-                return new Promise((resolve, reject) => {
-                    reject("Error with type: " + key);
-                })
-        }
-    };
-
-    const pushForm = (state, payload, res) => {
-        return new Promise((success, failed) => {
-            try {
-                console.log("cmd: pushForm. Payload: " + JSON.stringify(payload));
-                modal.show(state, { spinner: {} });
-                var options = {
-                    method: 'POST',
-                    headers: {
-                        'Content-Type': 'application/json;charset=utf-8'
-                    },
-                    body: JSON.stringify(state)
-                };
-                fetch(payload.fetchPath, options)
-                    .then(() => {
-                        dashboard.reloadCat();
-                        dismissModal();
-                        success("ok");
-                    })
-                    .catch((err) => {
-                        console.log(err);
-                        failed(err);
-                    });
-
-
-            } catch (error) {
-                console.log(error);
-                failed(error);
-            }
-        });
-    };
-
-    // const dismissModal = (state, payload, res) => {
-    //     return new Promise((success, failed) => {
-    //         try {
-    //             console.log("cmd: dismissModal. Payload: " + JSON.stringify(payload));
-    //             const modalData = modal.getData();
-    //             console.log("cmodalData: " + JSON.stringify(modalData));
-    //             modal.close();
-    //             success(modalData);
-    //         } catch (error) {
-    //             console.log(error);
-    //             failed(error);
-    //         }
-    //     });
-    // };
-
-    const getData = (state, payload, res) => {
-        return new Promise((resolve, reject) => {
-            resolve(state.inputs);
-        });
-    };
-
-    const getState = (state, payload, res) => {
-        return new Promise((resolve, reject) => {
-            resolve(state);
-        });
-    };
-
-    console.log(`cmds´(${JSON.stringify(pos)}): ${JSON.stringify(cmds)}`);
-
-    return new Promise((resolve, reject) => {
-        //A: Si ya ejecute todos los comandos termino
-        if (Object.keys(cmds).length <= pos) {
-            resolve(res);
-        } else {
-            var c = null;
-            var command = cmds[pos];
-            switch (command.type) {
-                case "parent-cmd":
-                    c = () => parentCmd(state, command.payload, res);
-                    break;
-                case "push-form":
-                    c = () => pushForm(state, command.payload, res);
-                    break;
-                case "dissmis-modal":
-                    c = () => dismissModal(state, command.payload, res);
-                    break;
-                case "get-state":
-                    c = () => getState(state, command.payload, res);
-                    break;
-                case "get-data":
-                    c = () => getData(state, command.payload, res);
-                    break;
-                default:
-                    console.log(`Cmd not found: ${command.type}`);
-                    c = () => new Promise((res, rej) => { rej(`Cmd not found: ${command.type}`) });
-                    break;
-            }
-
-            c()
-                .then((res) => {
-                    cmd(state, cmds, res, pos + 1);
-                })
-                .then((res) => resolve(res))
-                .catch(err => {
-                    console.log(err);
-                    reject(err);
-                });
-        }
-    })
-};
-
-const create = (newState, path) => {
-    try {
-        if (newState.type == "map") {
-            newState = utils.fillObjWithDflt(newState, dfltState);
-            newState.path = path;
-
-            Object.entries(newState.childs).forEach(child => {
-                switch (child[1].type) {
-                    case "modal":
-                        newState.childs[child[0]] = modal.create(child[1], path + "/" + child[0]);
-                        break;
-                    default:
-                        console.log("Error creating map child, incorrect type: " + child[1].type);
-                        break;
-                }
-            });
-            //console.log("Modal new State: " + JSON.stringify(newState));
-            states.push(newState);
-            return newState;
-        } else {
-            console.log("Error creating map, incorrect type: " + newState.type);
-            return null;
-        }
-    } catch (error) {
-        console.log(error);
-        return null;
-    }
-};
-
-const show = (state, parent) => {
-    const createFilters = () => {
+      const createFilters = () => {
         try {
             var divContainer =document.createElement("div");
             divContainer.className="container mx-auto mt-25";
@@ -232,42 +90,56 @@ const show = (state, parent) => {
         try {
 
           var divContainer = document.createElement("div");
-          divContainer.className="col-md-12 row noId-card-footer";
+          divContainer.className="col-md-12 container";
           cardParent.body.appendChild(divContainer);
 
           var divContent=document.createElement("div");
           divContent.className="col-lg-6";
           divContent.style.marginLeft="0px";
           divContent.style.paddingLeft="0px";
-          divContent.style.marginTop="0px";
+          divContent.style.marginTop="20px";
           divContainer.appendChild(divContent);
 
           var divCardContainer=document.createElement("div");
           divCardContainer.className="card l-bg-orange-dark";
-          divCardContainer.style.background="linear-gradient(to right, #0a504a, #38ef7d) !important"
-          divCardContainer.style.color="#fff"
           divContent.appendChild(divCardContainer)
 
 
-          var divContent = document.createElement("div");
-          divContent.style.fontSize ="110px";
-          divContent.style.lineHeight ="50px";
-          divContent.style.marginLeft ="15px";
-          divContent.style.color="#000";
-          divContent.style.right="5px";
-          divContent.style.top="20px";
-          divContent.style.opacity="0.1";
-          divCardContainer.appendChild(divContent);
 
-          var divMb=document.createElement("div");
-          divMb.className="mb-4";
-          divContent.appendChild(divMb);
+          var divContentCard = document.createElement("div");
+          divContentCard.className="card-statistic-3 p-4";
+          divCardContainer.appendChild(divContentCard);
+
+          var iconCard = document.createElement("div");
+          iconCard.className="card-icon card-icon-large";
+          iconCard.style.fontSize="110px";
+          iconCard.style.textAlign="center";
+          iconCard.style.lineHeight="50px";
+          iconCard.style.marginLeft="15px";
+          iconCard.style.color="#000";
+          iconCard.style.position="absolute";
+          iconCard.style.right="5px";
+          iconCard.style.top="20px";
+          iconCard.style.opacity="0,1";
+          divContentCard.appendChild(iconCard);
+
+          var icon =document.createElement("i");
+          icon.className="fas fa-truck-moving";
+          iconCard.appendChild(icon);
 
 
-         var text = document.createElement("h2");
-         text.innerHTML="Hola mundo";
+          var divText=document.createElement("div");
+          divText.className="mb-4";
+          divContentCard.appendChild(divText);
 
-         divMb.appendChild(text);
+          var text = document.createElement("h5");
+          text.className="card-title mb-0";
+          text.innerHTML="hola mundo";
+
+          divText.appendChild(text);
+
+
+
 
 
 
@@ -450,10 +322,13 @@ return(coords);
           console.log(error);
       }
   };
-    console.log("Map show: " + JSON.stringify(state));
-    const cardParent = card.create({ title: state.title }, parent);
-    createFilters();
-    drawCard();
-    createwidgets()
+        console.log("Map show: " + JSON.stringify(state));
+        const cardParent = card.create({ title: state.title }, parent);
+        createFilters();
+        drawCard();
+        createwidgets();
 
+    }
 };
+
+export default component;
